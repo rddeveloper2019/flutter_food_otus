865fa6352c4817c7dfcda0aa4c5703d3286f5294
Ridvan Dzh <63258916+rddeveloper2019@users.noreply.github.com>
2026-03-02 23:06:28 +0300

    add  foods json server

---
+++ foods_json_server/README.md
@@ -0,0 +1,15 @@
+# flutter_food_otus json server
+
+```
+cd foods_json_server
+```
+```
+npm i
+```
+```
+npm start
+```
+
+```
+http://localhost:4500/
+```
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_food_otus/theme/green_light_theme.dart';
 import 'package:flutter_food_otus/ui/recipes/recipes_page.dart';
+import 'package:http/http.dart' as http;
 
 void main() => runApp(const MyApp());
 
@@ -8,11 +11,39 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
+  @override
   Widget build(BuildContext context) {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      testRemoteApi();
+    });
     return MaterialApp(
       title: 'Otus Food',
       theme: GreenTheme.lightTheme,
       home: RecipesPage(),
     );
+  }
+}
+
+Future<void> testRemoteApi() async {
+  var client = http.Client();
+  final host = Platform.isAndroid ? '10.0.2.2' : 'localhost';
+  final url = Uri.http('$host:4500');
+
+  try {
+    var response = await client.get(
+      url,
+      headers: {'connection-type': 'application/json'},
+    );
+
+    if (response.statusCode != 200) {
+      throw ('❌ Ошибка HTTP: ${response.statusCode}');
+    }
+    print('🔌 SocketException: OK');
+  } on SocketException catch (e) {
+    print('🔌 SocketException: ${e.message}');
+  } catch (e) {
+    print('❗ Ошибка: $e');
+  } finally {
+    client.close();
   }
 }
